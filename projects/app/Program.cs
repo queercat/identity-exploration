836202f9803ce9a0dfbc85fcb2ffb4aa588118ad
May Tusek <tusek.may@gmail.com>
2023-07-31 23:36:15 -0700

    Upgrades .NET

--- projects/app/Program.cs
+++ projects/app/Program.cs
@@ -7,15 +7,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication(
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.UseSwagger();
-  app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
