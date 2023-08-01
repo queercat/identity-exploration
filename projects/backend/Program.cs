4be5bdba66ada410c76e2560f638d6e1c0597b41
May Tusek <tusek.may@gmail.com>
2023-07-31 23:26:18 -0700

    Begins adding JWT.

--- projects/backend/Program.cs
+++ projects/backend/Program.cs
@@ -7,13 +7,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication(
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
